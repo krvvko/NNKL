a71371da6fc7bdc08aa939281bc07aced9f78c74
krvvko_huh <krvvko@gmail.com>
2022-02-09 20:01:46 -0500

    Russian translit language added

--- NoNumbersKB.php
+++ NoNumbersKB.php
@@ -6,7 +6,13 @@ class NoNumbersKB
     function zero() {
         return 0;
     }
+    function nol() {
+        return 0;
+    }
     function one() {
+        return 1;
+    }
+    function odin() {
         return 1;
     }
 
@@ -14,7 +20,15 @@ class NoNumbersKB
         return 2;
     }
 
+    function dva() {
+        return 2;
+    }
+
     function three() {
+        return 3;
+    }
+
+    function tri() {
         return 3;
     }
 
@@ -22,7 +36,15 @@ class NoNumbersKB
         return 4;
     }
 
+    function chetire() {
+        return 4;
+    }
+
     function five() {
+        return 5;
+    }
+
+    function piat() {
         return 5;
     }
 
@@ -30,7 +52,15 @@ class NoNumbersKB
         return 6;
     }
 
+    function shest() {
+        return 6;
+    }
+
     function seven() {
+        return 7;
+    }
+
+    function sem() {
         return 7;
     }
 
@@ -38,11 +68,23 @@ class NoNumbersKB
         return 8;
     }
 
+    function vosem() {
+        return 8;
+    }
+
     function nine() {
         return 9;
     }
 
+    function deviat() {
+        return 9;
+    }
+
     function ten() {
+        return 10;
+    }
+
+    function deciat() {
         return 10;
     }
 
